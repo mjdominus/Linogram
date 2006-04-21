d199bcd9fb0a5b7a51da9b6025c124b915687a93
mjd <mjd>
2006-04-21 14:59:31 +0000

    Numbers in test outputs are now rounded off to six digits after the decimal point to prevent spurious failures caused by floating-point slop

--- lib/testutils/dump_hash.pl
+++ lib/testutils/dump_hash.pl
@@ -3,7 +3,11 @@ sub dump_hash {
 #  my @a = %$h;
 #  warn "dump_hash <@a>...\n";
   for my $k (sort keys %$h) {
-    print "$k: $h->{$k}\n";
+    my $v = $h->{$k};
+    if ($v != 0 && $v =~ /^-?\d*\.\d{7,}$/) {
+      $v = sprintf("%.6f", $v);
+    }
+    print "$k: $v\n";
   }
 }
 
