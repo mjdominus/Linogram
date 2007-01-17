455577f7ff72931dd1f447a46cefeccba0fa4a46
mjd <mjd>
2007-01-17 15:47:56 +0000

    tests for compound names in subscripts

--- lib/testutils/names.pl
+++ lib/testutils/names.pl
@@ -5,6 +5,8 @@ use Name;
 my $c12 = Expression->new('CON', 12);
 my $vi  = Expression->new('VAR', my $ni = Name->new("i"));
 my $vj  = Expression->new('VAR', my $nj = Name->new("j"));
+my $vbi  = Expression->new('VAR', my $ni = Name->new("b", "i"));
+my $vbj  = Expression->new('VAR', my $ni = Name->new("b", "j"));
 
 @names =
   (
@@ -20,11 +22,18 @@ my $vj  = Expression->new('VAR', my $nj = Name->new("j"));
    Name->new(["fred", $vj], "x"),          # fred[j].x
    Name->new(["fred", $vj], ["x", $c12]),  # fred[j].x[12]
    Name->new(["fred", $vj], ["x", $vi]),   # fred[j].x[i]
+   Name->new(["fred", $vbi]),               # fred[b.i]
+   Name->new("fred", ["x", $vbi]),          # fred.x[b.i]
+   Name->new(["fred", $c12], ["x", $vbi]),  # fred[12].x[b.i]
+   Name->new(["fred", $vbj], "x"),          # fred[b.j].x
+   Name->new(["fred", $vbj], ["x", $c12]),  # fred[b.j].x[12]
+   Name->new(["fred", $vbj], ["x", $vbi]),   # fred[b.j].x[b.i]
   );
 
 %parts = (c12 => $c12,
           vi => $vi, vj => $vj,
           ni => $ni, nj => $nj,
+          vbi => $vbi, vbj => $vbj,
          );
 
 sub V { Expression->new(N(@_)) }
--- u/Name__substitute_subscripts.t
+++ u/Name__substitute_subscripts.t
@@ -3,17 +3,21 @@ use lib './lib', './lib/testutils';
 use vars '@names';
 require 'names.pl';
 
-use Test::More tests => 12;
+use Test::More tests => 18;
 
 use Environment;
-my $env = Environment->new(i => 4, j => 3);
+my $env = Environment->new(i => 4, j => 3, 
+		'b.i' => 5, 'b.j' => 6
+	);
 
-for my $i (0 .. 11) {
+for my $i (0 .. 17) {
   my $b = $names[$i];
   my $a = $b->substitute_subscripts($env);
   my $bstr = $b->to_str;
   my $astr = $a->to_str;
   my $xstr = $bstr;
+  $xstr =~ s/b\.i/5/g;
+  $xstr =~ s/b\.j/6/g;
   $xstr =~ s/i/4/g;
   $xstr =~ s/j/3/g;
   is($astr, $xstr, $bstr);
